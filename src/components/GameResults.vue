80b9479aea822472ce6d6dcf6bc571c4a31796cf
John Knotts <jhnnyk@gmail.com>
2022-06-30 11:35:30 -0600

    finishing touches

--- src/components/GameResults.vue
+++ src/components/GameResults.vue
@@ -1,11 +1,39 @@
 <template>
-  <p>Reaction time: {{ score }} milliseconds</p>
+  <p>
+    Reaction time: <br />
+    <span class="rank">
+      {{ score }} ms <br />
+      {{ rank }}
+    </span>
+  </p>
 </template>
 
 <script>
 export default {
   props: ['score'],
+
+  data() {
+    return {
+      rank: null,
+    };
+  },
+
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "You're quick!";
+    } else if (this.score < 400) {
+      this.rank = 'Good job!';
+    } else {
+      this.rank = '...like a snail :-(';
+    }
+  },
 };
 </script>
 
-<style></style>
+<style>
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
